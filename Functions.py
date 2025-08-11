34712eada57f9616602803be57f5d8bca1a67d77
the-goldstandard <michaelchiu2002@gmail.com>
2025-08-11 17:56:48 +0100

    Update Functions.py
    v2.0.0

--- Functions.py
+++ Functions.py
@@ -5,13 +5,13 @@ def CoinCountProbabilities(race_or_KO):
     # This function uses queueing theory to find the probabilities of all the coin counts from 0 to 20 during a race in Mario Kart World.
     # It requires a boolean input to indicate whether 3-lap races (True) or 6-lap knockout tours (False) are considered.
 
-    file=pyxl.load_workbook("Mario Kart World race coin and crash data.xlsx",data_only=True)
+    file=pyxl.load_workbook("Mario Kart World race data.xlsx",data_only=True)
     spreadsheet=file["Sheet1"]
-    Lambda=spreadsheet.cell(row=2,column=9).value
+    Lambda=spreadsheet.cell(row=2,column=16).value
     # The collection rate of coins (1/min)
-    mu=spreadsheet.cell(row=3,column=9).value
+    mu=spreadsheet.cell(row=3,column=16).value
     # The frequency of the player hit by an item and loses up to 3 coins each time (1/min)
-    r=spreadsheet.cell(row=4,column=9).value*(3+0.5*race_or_KO)/3.5
+    r=spreadsheet.cell(row=11,column=16).value*(3+0.5*race_or_KO)/3.5
     # The frequency of the player finishing the race and returning to the starting coin count between 0 and 5 (1/min)
     # The expected duration of a 6-lap knockout tour is around 3.5 laps (i.e. 1/6*6? = 21/6 = 3.5)
 
